3f67d39c546b20341499fcc1261cb002f634f923
ezhh <owlecho@live.com>
2018-01-07 20:18:26 +0000

    Tidy code

--- init.lua
+++ init.lua
@@ -1,24 +1,19 @@
 
 -- register bypass priv
 minetest.register_privilege("news_bypass", {
-	description = "Skip the news.", give_to_singleplayer=false})
+	description = "Skip the news.", give_to_singleplayer = false})
 
 -- create formspec from text file
 local function get_formspec()
-
 	local news_file = io.open(minetest.get_worldpath().."/news.txt", "r")
-	
-	local news_fs = 'size[12,8.5]'
+	local news_fs = 'size[12,8.5]'..
+		"button_exit[-0.05,8.05;2,1;exit;Close]"
 	if news_file then
 		local news = news_file:read("*a")
+		news_file:close()
 		news_fs = news_fs.."textarea[0.25,0.25;12,9;news;;"..news.."]"
 	else
 		news_fs = news_fs.."textarea[0.25,0.25;12,9;news;;No current news.]"
-	end
-	news_fs = news_fs.."button_exit[-0.05,8.05;2,1;exit;Close]"
-
-	if news_file then
-		news_file:close()
 	end
 	return news_fs
 end
@@ -36,7 +31,7 @@ end)
 -- command to display server news at any time
 minetest.register_chatcommand("news", {
 	description = "Shows server news to the player",
-	func = function (name, params)
+	func = function (name)
 		local player = minetest.get_player_by_name(name)
 		minetest.show_formspec(name, "news", get_formspec())	
 	end
