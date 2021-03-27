eab4b15e3b580e775f4dbe69759e479cfede3f20
Ronoaldo JLP <ronoaldo@gmail.com>
2021-03-27 00:49:27 -0300

    Localization support for server news, UI tweaks.
    Added suppport to show multi-language server news
    based on client language settings, falling back english
    and also falling back to the original news.txt file
    from original mod.
    
    Minor UI tweaks, like making the textarea non-editable
    (which improves readability) and making the text font
    monospaced, so you have a small ammount of formatting
    control in messages while better rendering is not added.

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-Server News
-===
+# minenews
 
-Simple server news mod by Shara RedCat.
+Localized Simple server news for [Minetest](https://www.minetest.net/) servers.
+Original version by Shara RedCat, forked and improved by Ronoaldo.
 
-This displays a news formspec when the player joins. To set the news text make a file named news.txt in your world directory. The contents of this file will display as the news. When changing the contents of the file, there is no need to restart the server for it to update in game.
+This displays a news formspec when the player joins. To set the news text
+make a file named `news_*lang_code*.txt` in your world directory, falling back
+to a `news.txt` file.
 
-Players can also type /news to display the formspec at any time.
+The contents of this file will display as the news. When changing the contents
+of the file, there is no need to restart the server for it to update in game.
 
-Players with the news_bypass privilege will not see the formspec when they sign in, but can still use the /news command.
+Players can also type `/news` to display the formspec at any time.
+
+Players with the news\_bypass privilege will not see the formspec when they
+sign in, but can still use the /news command.
 
 
-License
--------
+# License
 
 Code for this mod is released under MIT (https://opensource.org/licenses/MIT).
--- init.lua
+++ init.lua
@@ -1,30 +1,49 @@
-
--- register bypass priv
+-- Register bypass priv
 minetest.register_privilege("news_bypass", {
-	description = "Skip the news.", give_to_singleplayer = false})
+	description = "Skip the news.",
+	give_to_singleplayer = false
+})
 
--- create formspec from text file
-local function get_formspec()
-	local news_file = io.open(minetest.get_worldpath().."/news.txt", "r")
+-- Create formspec from text file
+local function get_formspec(name)
+	-- Lookup player language preference
+	local player_info = minetest.get_player_information(name)
+	local lang = player_info.lang_code
+	if lang == "" then
+		lang = "en"
+	end
+	-- Lookup news file to display, trying by language first, with a default news.txt
+	-- fallback.
+	local news_filename = minetest.get_worldpath().."/news_"..lang..".txt", "r"
+	local news_file = io.open(news_filename, "r")
+	if not news_file then
+		news_filename = minetest.get_worldpath().."/news.txt", "r"
+		news_file = io.open(news_filename, "r")
+	end
+	minetest.log("verbose",
+		"Displaying news to player "..name.." in "..lang.." from file "..news_filename)
+	-- Display the formspec for the server news
 	local news_fs = 'size[12,8.25]'..
-		"button_exit[-0.05,7.8;2,1;exit;Close]"
+		"style_type[textarea;font=mono]"..
+		"button_exit[-0.05,7.8;2,0.8;exit;OK]"
 	if news_file then
 		local news = news_file:read("*a")
 		news_file:close()
-		news_fs = news_fs.."textarea[0.25,0;12.1,9;news;;"..minetest.formspec_escape(news).."]"
+		news_fs = news_fs.."textarea[0.25,0;12.1,9;;;"..minetest.formspec_escape(news).."]"
 	else
-		news_fs = news_fs.."textarea[0.25,0;12.1,9;news;;No current news.]"
+		news_fs = news_fs.."textarea[0.25,0;12.1,9;;;No current news.]"
 	end
+	minetest.log("verbose", "news_fs => "..news_fs)
 	return news_fs
 end
 
--- show news formspec on player join, unless player has bypass priv
+-- Show news formspec on player join, unless player has bypass priv
 minetest.register_on_joinplayer(function (player)
 	local name = player:get_player_name()
 	if minetest.get_player_privs(name).news_bypass then
 		return
 	else
-		minetest.show_formspec(name, "news", get_formspec())
+		minetest.show_formspec(name, "news", get_formspec(name))
 	end
 end)
 
@@ -33,6 +52,6 @@ minetest.register_chatcommand("news", {
 	description = "Shows server news to the player",
 	func = function (name)
 		local player = minetest.get_player_by_name(name)
-		minetest.show_formspec(name, "news", get_formspec())	
+		minetest.show_formspec(name, "news", get_formspec(name))
 	end
 })
